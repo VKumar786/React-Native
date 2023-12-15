2caae3601920919d56000ad7a80d910fdda33600
VKumar786 <vkumar@onemindservices.com>
2023-12-16 02:34:43 +0530

    tut19

--- tut18/rn-starter-main/src/screens/AboutScreen.js
+++ tut18/rn-starter-main/src/screens/AboutScreen.js
@@ -1,23 +1,28 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, FlatList } from "react-native";
 
 const AboutScreen = () => {
-    let arr = [
-        {name: "vishal"},
-        {name: "papa ji"},
-        {name: "vivek"},
-    ]
-    return (
-        <View>
-            {arr.map((item) => {
-                return <Text>{item.name}</Text>
-            })}
-        </View> 
-    )
+	let arr = [
+		{ name: "vishal" },
+		{ name: "suraj" },
+		{ name: "vivek" },
+	]
+	return (
+		<View>
+			<FlatList data={arr} renderItem={(element) => {
+				// element { "item" : { name : "vishal" }, "index": "0", "separators": {}}
+				return <Text>{JSON.stringify(element)}</Text>
+			}} />
+			<FlatList data={arr} renderItem={({ item, index }) => {
+				// element { "item" : { name : "vishal" }, "index": "0", "separators": {}}
+				return <Text key={index}>{item.name}</Text>
+			}} />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    textStyle: {}
+	textStyle: {}
 })
 
 export default AboutScreen  
