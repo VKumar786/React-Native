101d4e3298b93e5ddebe1433d88e81e1c3a00a90
vkumar786 <vkumar@onemindservices.com>
2023-12-21 18:00:29 +0530

    tutconfig

--- GitAutomation.py
+++ GitAutomation.py
@@ -11,7 +11,7 @@ def git_push(message, branch):
         print(f"Error executing Git commands: {e}")
 
 
-el = 39
+el = "config"
 
 
 message = "tut" + str(el)
---
+++ tut21/rn-starter-main/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { FlatList, Text, StyleSheet, Button, Linking, View } from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return <View>
+  </View>
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    color: "white"
+  },
+  buttonStyle: {
+    margin: 10,
+    backgroundColor: "#000",
+    color: "#fff",
+    padding: 5,
+  }
+});
+
+export default HomeScreen;
